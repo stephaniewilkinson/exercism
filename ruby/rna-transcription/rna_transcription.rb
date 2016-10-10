aa63ebf15342af97057b2b3d92eaba3eaf1e58b5
Stephanie Wilkinson <what.happens@gmail.com>
2016-10-10 15:51:58 -0700

    removes unnecessary return

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,7 +1,7 @@
 class Complement
   def self.of_dna(strand)
-    strand.each_char {|c| /[GCTA]/ =~c ? true : (return "")}
-    return strand.tr('GCTA', 'CGAU')
+    strand.each_char {|c| /[CGTA]/ =~c ? true : (return "")}
+    strand.tr('GCTA', 'CGAU')
   end
 end
 
