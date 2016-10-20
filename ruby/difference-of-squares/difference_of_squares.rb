b1e130fde9c2099f2cdee27f74b470188b0d60f1
Stephanie Wilkinson <what.happens@gmail.com>
2016-10-19 20:59:13 -0700

    diff of squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,8 +1,23 @@
+require 'pry'
+
 class Squares
-  def initialize param
+  def initialize arg
+    @arg = arg
   end
 
   def square_of_sum
-    225
+    sum = 0
+    (1..@arg).each { |i| sum += i }
+    sum ** 2
+  end
+
+  def sum_of_squares
+    sum = 0
+    (1..@arg).each { |i| sum += (i**2) }
+    sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
   end
 end
--- ruby/difference-of-squares/difference_of_squares_test.rb
+++ ruby/difference-of-squares/difference_of_squares_test.rb
@@ -13,52 +13,42 @@ class DifferenceOfSquaresTest < Minitest::Test
   end
 
   def test_square_of_sum_10
-    skip
     assert_equal 3_025, Squares.new(10).square_of_sum
   end
 
   def test_square_of_sum_100
-    skip
     assert_equal 25_502_500, Squares.new(100).square_of_sum
   end
 
   def test_sum_of_squares_5
-    skip
     assert_equal 55, Squares.new(5).sum_of_squares
   end
 
   def test_sum_of_squares_10
-    skip
     assert_equal 385, Squares.new(10).sum_of_squares
   end
 
   def test_sum_of_squares_100
-    skip
     assert_equal 338_350, Squares.new(100).sum_of_squares
   end
 
   def test_difference_of_squares_0
-    skip
     assert_equal 0, Squares.new(0).difference
   end
 
   def test_difference_of_squares_5
-    skip
     assert_equal 170, Squares.new(5).difference
   end
 
   def test_difference_of_squares_10
-    skip
     assert_equal 2_640, Squares.new(10).difference
   end
 
   def test_difference_of_squares_100
-    skip
     assert_equal 25_164_150, Squares.new(100).difference
   end
 
   def test_consistent_difference
-    skip
     squares = Squares.new(10)
     assert_equal squares.difference, squares.difference
   end
